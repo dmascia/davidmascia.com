60feb34d167748e189c607a401624c6149ababe8
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 16:47:59 +0000

    Mon, Jan 13, 2025, 11:47 AM -05:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import Layout from "@/components/Layout/layout";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -6,22 +8,32 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+
 export default function Home() {
+  const [value, setValue] = useState(0);
   return (
     <>
       <Layout>
         <>
-          <BottomNavigation
-            showLabels
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-          >
-            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-          </BottomNavigation>
+          <Box sx={{ width: 500 }}>
+            <BottomNavigation
+              showLabels
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            >
+              <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+              <BottomNavigationAction
+                label="Favorites"
+                icon={<FavoriteIcon />}
+              />
+              <BottomNavigationAction
+                label="Nearby"
+                icon={<LocationOnIcon />}
+              />
+            </BottomNavigation>
+          </Box>
         </>
       </Layout>
     </>
