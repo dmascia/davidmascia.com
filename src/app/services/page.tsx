1e5349fadca60e31d42365f14be8d44883fa6d89
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 18:56:34 +0000

    Mon, Jan 13, 2025, 1:56 PM -05:00

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -33,45 +33,72 @@ export default function Home() {
     <Layout>
       <Container>
         <Box mt={8}>
-          <Box
+          <Grid
+            container
+            spacing={6}
+            columns={12}
+            direction="row"
             sx={{
-              width: "100%",
-              display: "grid",
-              gridTemplateColumns:
-                "repeat(auto-fill, minmax(min(200px, 100%), 1fr))",
-              gap: 2,
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            {cards.map((card, index) => (
-              <Card>
-                <CardActionArea
-                  onClick={() => setSelectedCard(index)}
-                  data-active={selectedCard === index ? "" : undefined}
+            <Grid size={{ xs: 12, md: 10 }}>
+              <Box>
+                <Typography variant="h2" component="h1">
+                  Services
+                </Typography>
+
+                <Typography variant="h6" component="h2">
+                  I specialize in designing and building high-performance,
+                  user-focused applications that scale effortlessly. My
+                  extensive experience ensures that your project is not only
+                  functional but also optimized for success. Here’s how I can
+                  help:
+                </Typography>
+                <Box
                   sx={{
-                    height: "100%",
-                    "&[data-active]": {
-                      backgroundColor: "action.selected",
-                      "&:hover": {
-                        backgroundColor: "action.selectedHover",
-                      },
-                    },
+                    width: "100%",
+                    display: "grid",
+                    gridTemplateColumns:
+                      "repeat(auto-fill, minmax(min(200px, 100%), 1fr))",
+                    gap: 2,
                   }}
                 >
-                  <CardContent sx={{ height: "100%" }}>
-                    <Typography variant="h5" component="div">
-                      {card.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {card.description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            ))}
-          </Box>
-          <Box pt={4}>
-            <LetsConnect />
-          </Box>{" "}
+                  {cards.map((card, index) => (
+                    <Card>
+                      <CardActionArea
+                        onClick={() => setSelectedCard(index)}
+                        data-active={selectedCard === index ? "" : undefined}
+                        sx={{
+                          height: "100%",
+                          "&[data-active]": {
+                            backgroundColor: "action.selected",
+                            "&:hover": {
+                              backgroundColor: "action.selectedHover",
+                            },
+                          },
+                        }}
+                      >
+                        <CardContent sx={{ height: "100%" }}>
+                          <Typography variant="h5" component="div">
+                            {card.title}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {card.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  ))}
+                </Box>
+              </Box>
+
+              <Box pt={4}>
+                <LetsConnect />
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
       </Container>
     </Layout>
