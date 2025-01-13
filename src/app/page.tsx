2946896d63e7c86baf219dffb9dd4e9810e0b50c
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 16:46:10 +0000

    Mon, Jan 13, 2025, 11:46 AM -05:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
 import Layout from "@/components/Layout/layout";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 export default function Home() {
   return (
     <>
       <Layout>
-        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-          wcdcfd
-        </Box>
+        <>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
+        </>
       </Layout>
     </>
   );
