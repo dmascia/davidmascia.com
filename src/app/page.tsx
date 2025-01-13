83303941af62825e6e69dba27a9a969bfb51ec7c
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 17:02:23 +0000

    Mon, Jan 13, 2025, 12:02 PM -05:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,23 +16,20 @@ export default function Home() {
     <>
       <Layout>
         <>
-          {/* <Container maxWidth="m"> */}
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-              <Grid size={{ xs: 6, md: 8 }}>
-                <p>xs=6 md=8</p>
-              </Grid>
-              <Grid size={{ xs: 6, md: 4 }}>
-                <p>xs=6 md=4</p>
-              </Grid>
-              <Grid size={{ xs: 6, md: 4 }}>
-                <p>xs=6 md=4</p>
-              </Grid>
-              <Grid size={{ xs: 6, md: 8 }}>
-                <p>xs=6 md=8</p>
-              </Grid>
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 6, md: 8 }}>
+              <p>xs=6 md=8</p>
             </Grid>
-          </Box>
+            <Grid size={{ xs: 6, md: 4 }}>
+              <p>xs=6 md=4</p>
+            </Grid>
+            <Grid size={{ xs: 6, md: 4 }}>
+              <p>xs=6 md=4</p>
+            </Grid>
+            <Grid size={{ xs: 6, md: 8 }}>
+              <p>xs=6 md=8</p>
+            </Grid>
+          </Grid>
           {/* </Container> */}
           <Box sx={{ width: 500 }}>
             <BottomNavigation
