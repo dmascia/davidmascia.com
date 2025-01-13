c9ed03b934e5bc3fbe587fd8ba72d740e8c515da
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 17:38:04 +0000

    Mon, Jan 13, 2025, 12:38 PM -05:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,27 +15,36 @@ import Image from "next/image";
 export default function Home() {
   const [value, setValue] = useState(0);
   return (
-    <>
-      <Layout>
-        <>
-          <Box
-            display="flex"
-            justifyContent="center"
-            minWidth="100vw"
-            m={8}
-            pt={3}
+    <Layout>
+      <Container>
+        <Box display="flex" justifyContent="center" mt={8}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
-            <Grid
-              container
-              rowSpacing={1}
-              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            >
-              <Grid size={6}>
-                <Typography variant="h1" component="h3">
-                  Hi, I’m David Mascia.
-                </Typography>
-              </Grid>
-              <Grid size={6}>
+            <Grid size={6}>
+              <Typography variant="h1" component="h1">
+                Hi, I’m David Mascia.
+              </Typography>
+              <Typography variant="h6" component="h2">
+                Software Engineer | Problem Solver | Innovator
+              </Typography>
+              <Typography variant="p" component="p">
+                I am a seasoned Lead Software Engineer with a proven track
+                record of improving user engagement, performance, and
+                operational efficiency. At Volume, I boosted engagement by 85%
+                and frontend performance by 75% through optimized UI/UX with
+                Next.js and TypeScript and backend improvements with Python and
+                Django. At CardCash, I spearheaded the transition to
+                microservices, achieving a 4x user growth and an 80% revenue
+                increase. Passionate about building scalable, user-centric
+                applications, I excel at driving innovation, mentoring teams,
+                and delivering measurable results.
+              </Typography>
+            </Grid>
+            <Grid size={6}>
+              <Box sx={{ display: { xs: "none", md: "flex" } }}>
                 <Image
                   src={
                     "https://media.licdn.com/dms/image/v2/D4E03AQH2YwOvFWBL4A/profile-displayphoto-shrink_800_800/B4EZRNjlKuGwAc-/0/1736467961931?e=1742428800&v=beta&t=64e_qMWpJ4wzltQqKS2cb-jEyGgksRGP-KMjv2R_XKI"
@@ -46,64 +55,23 @@ export default function Home() {
                   height="400"
                   style={{ borderRadius: "50px" }}
                 />
-              </Grid>
-              <Grid size={6}>
-                <p>3</p>
-              </Grid>
-              <Grid size={6}>
-                <p>4</p>
-              </Grid>
+              </Box>
+              <Box sx={{ display: { xs: "flex", md: "none" }, mt: 6, ml: 2 }}>
+                <Image
+                  src={
+                    "https://media.licdn.com/dms/image/v2/D4E03AQH2YwOvFWBL4A/profile-displayphoto-shrink_800_800/B4EZRNjlKuGwAc-/0/1736467961931?e=1742428800&v=beta&t=64e_qMWpJ4wzltQqKS2cb-jEyGgksRGP-KMjv2R_XKI"
+                  }
+                  alt="David Mascia"
+                  sizes="100vw"
+                  width="150"
+                  height="150"
+                  style={{ borderRadius: "50px" }}
+                />
+              </Box>
             </Grid>
-            {/* <Grid container spacing={2}>
-              <Grid ize={{ xs: 6, md: 8 }}>
-                <Typography variant="h1" component="h3">
-                  Hi, I’m David Mascia.
-                </Typography>
-              </Grid>
-              <Grid size={{ xs: 6, md: 4 }}>
-                <Box sx={{ display: { xs: "flex", md: "flex" }, mr: 2 }}>
-                  <Image
-                    src={
-                      "https://media.licdn.com/dms/image/v2/D4E03AQH2YwOvFWBL4A/profile-displayphoto-shrink_800_800/B4EZRNjlKuGwAc-/0/1736467961931?e=1742428800&v=beta&t=64e_qMWpJ4wzltQqKS2cb-jEyGgksRGP-KMjv2R_XKI"
-                    }
-                    alt="David Mascia"
-                    sizes="100vw"
-                    width="100"
-                    height="100"
-                    style={{ borderRadius: "50px" }}
-                  />
-                </Box>
-              </Grid>
-              <Grid size={{ xs: 6, md: 4 }}>
-                <p>xs=6 md=4</p>
-              </Grid>
-              <Grid size={{ xs: 6, md: 8 }}>
-                <p>xs=6 md=8</p>
-              </Grid>
-            </Grid> */}
-          </Box>
-          {/* </Container> */}
-          <Box sx={{ width: 500 }}>
-            <BottomNavigation
-              showLabels
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }}
-            >
-              <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-              <BottomNavigationAction
-                label="Favorites"
-                icon={<FavoriteIcon />}
-              />
-              <BottomNavigationAction
-                label="Nearby"
-                icon={<LocationOnIcon />}
-              />
-            </BottomNavigation>
-          </Box>
-        </>
-      </Layout>
-    </>
+          </Grid>
+        </Box>
+      </Container>
+    </Layout>
   );
 }
