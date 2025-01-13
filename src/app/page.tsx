dbc3241944bb7daac0137b109fc4b58449f9815c
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 17:49:10 +0000

    Mon, Jan 13, 2025, 12:49 PM -05:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,46 +4,49 @@ import Layout from "@/components/Layout/layout";
 import Grid from "@mui/material/Grid2";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
 
 export default function Home() {
-  const [value, setValue] = useState(0);
   return (
     <Layout>
       <Container>
-        <Box display="flex" justifyContent="center" mt={8}>
+        <Box mt={8}>
           <Grid
             container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            spacing={6}
+            columns={12}
+            direction="row"
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           >
-            <Grid size={6}>
+            <Grid size={{ xs: 12, md: 6 }}>
               <Typography variant="h1" component="h1">
-                Hi, I’m David Mascia.
+                Hi, I’m ...
+              </Typography>
+              <Typography variant="h1" component="h1">
+                David Mascia
               </Typography>
               <Typography variant="h6" component="h2">
-                Software Engineer | Problem Solver | Innovator
+                Software Engineer | Problem Solver
               </Typography>
               <Typography variant="p" component="p">
-                I am a seasoned Lead Software Engineer with a proven track
-                record of improving user engagement, performance, and
-                operational efficiency. At Volume, I boosted engagement by 85%
-                and frontend performance by 75% through optimized UI/UX with
-                Next.js and TypeScript and backend improvements with Python and
-                Django. At CardCash, I spearheaded the transition to
-                microservices, achieving a 4x user growth and an 80% revenue
-                increase. Passionate about building scalable, user-centric
-                applications, I excel at driving innovation, mentoring teams,
-                and delivering measurable results.
+                I am an experienced Software Engineer with over 20 years of
+                hands-on expertise and a proven history of enhancing user
+                engagement, optimizing performance, and improving operational
+                efficiency. At Volume, I increased user engagement by 85% and
+                improved frontend performance by 75% by refining the UI/UX with
+                Next.js and TypeScript, alongside backend enhancements using
+                Python and Django. At CardCash, I led the migration to a
+                microservices architecture, driving a 4x growth in users and an
+                80% revenue boost. Committed to developing scalable,
+                user-focused applications, I thrive on fostering innovation,
+                mentoring teams, and delivering impactful, measurable outcomes.
               </Typography>
             </Grid>
-            <Grid size={6}>
+            <Grid size={{ xs: 0, md: 4 }}>
               <Box sx={{ display: { xs: "none", md: "flex" } }}>
                 <Image
                   src={
@@ -53,18 +56,6 @@ export default function Home() {
                   sizes="100vw"
                   width="400"
                   height="400"
-                  style={{ borderRadius: "50px" }}
-                />
-              </Box>
-              <Box sx={{ display: { xs: "flex", md: "none" }, mt: 6, ml: 2 }}>
-                <Image
-                  src={
-                    "https://media.licdn.com/dms/image/v2/D4E03AQH2YwOvFWBL4A/profile-displayphoto-shrink_800_800/B4EZRNjlKuGwAc-/0/1736467961931?e=1742428800&v=beta&t=64e_qMWpJ4wzltQqKS2cb-jEyGgksRGP-KMjv2R_XKI"
-                  }
-                  alt="David Mascia"
-                  sizes="100vw"
-                  width="150"
-                  height="150"
                   style={{ borderRadius: "50px" }}
                 />
               </Box>
