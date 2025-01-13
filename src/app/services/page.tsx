617d8db8c205e7957b95c8980ea47d054387fca1
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-13 18:54:21 +0000

    Mon, Jan 13, 2025, 1:54 PM -05:00

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,235 +1,77 @@
 "use client";
+import * as React from "react";
 import Layout from "@/components/Layout/layout";
 import Grid from "@mui/material/Grid2";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import LetsConnect from "@/components/LetsConnect/";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineDot from "@mui/lab/TimelineDot";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
+const cards = [
+  {
+    id: 1,
+    title: "Plants",
+    description: "Plants are essential for all life.",
+  },
+  {
+    id: 2,
+    title: "Animals",
+    description: "Animals are a part of nature.",
+  },
+  {
+    id: 3,
+    title: "Humans",
+    description: "Humans depend on plants and animals for survival.",
+  },
+];
 
 export default function Home() {
+  const [selectedCard, setSelectedCard] = React.useState(0);
   return (
     <Layout>
       <Container>
         <Box mt={8}>
-          <Grid
-            container
-            spacing={6}
-            columns={12}
-            direction="row"
+          <Box
             sx={{
-              justifyContent: "center",
-              alignItems: "center",
+              width: "100%",
+              display: "grid",
+              gridTemplateColumns:
+                "repeat(auto-fill, minmax(min(200px, 100%), 1fr))",
+              gap: 2,
             }}
           >
-            <Grid size={{ xs: 12, md: 10 }}>
-              <Box>
-                <Typography variant="h2" component="h1">
-                  Services
-                </Typography>
-
-                <Typography variant="h6" component="h2">
-                  I specialize in designing and building high-performance,
-                  user-focused applications that scale effortlessly. My
-                  extensive experience ensures that your project is not only
-                  functional but also optimized for success. Here’s how I can
-                  help:
-                </Typography>
-                <Timeline position="alternate">
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                      <strong>Frontend Development</strong>
-                      <br /> <br />
-                      Delivering responsive, intuitive, and visually stunning
-                      user interfaces. <br />
-                      <br /> Expertise in React, Next.js, and TypeScript. <br />{" "}
-                      Accessible, mobile-first designs optimized for
-                      performance. <br /> Seamless user experiences across
-                      devices and browsers.
-                    </TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                      <strong>Backend Development</strong>
-                      <br /> <br />
-                      Building reliable, scalable, and efficient server-side
-                      solutions.
-                      <br />
-                      <br /> Proficient in Python, Django, and Node.js. <br />
-                      API development, integration, and optimization. <br />
-                      Focus on performance, security, and maintainability.
-                    </TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                      {" "}
-                      Software Engineer, Scivantage <br />
-                      SEP 2012 – APR 2015
-                    </TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                      {" "}
-                      Software Engineer (Contract), Digital Music Pool <br />
-                      MAY 2008 – JUL 2012
-                    </TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                      {" "}
-                      Software Engineer, Focus Camera <br />
-                      JUL 2005 – APR 2008
-                    </TimelineContent>
-                  </TimelineItem>
-                </Timeline>
-                <Typography variant="h6" component="h2">
-                  <br />
-                  SKILLS
-                </Typography>
-                <Timeline position="alternate">
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>TypeScript</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>JavaScript</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Python3</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>HTML5</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>CSS3</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Django</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Next.js</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>React</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Redux</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Webpack</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Storybook</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Jest</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>SQL</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Postgres</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Docker</TimelineContent>
-                  </TimelineItem>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                    </TimelineSeparator>
-                    <TimelineContent>Git</TimelineContent>
-                  </TimelineItem>
-                </Timeline>
-              </Box>
-
-              <Box pt={4}>
-                <LetsConnect />
-              </Box>
-            </Grid>
-          </Grid>
+            {cards.map((card, index) => (
+              <Card>
+                <CardActionArea
+                  onClick={() => setSelectedCard(index)}
+                  data-active={selectedCard === index ? "" : undefined}
+                  sx={{
+                    height: "100%",
+                    "&[data-active]": {
+                      backgroundColor: "action.selected",
+                      "&:hover": {
+                        backgroundColor: "action.selectedHover",
+                      },
+                    },
+                  }}
+                >
+                  <CardContent sx={{ height: "100%" }}>
+                    <Typography variant="h5" component="div">
+                      {card.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {card.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
+          </Box>
+          <Box pt={4}>
+            <LetsConnect />
+          </Box>{" "}
         </Box>
       </Container>
     </Layout>
