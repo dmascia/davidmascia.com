a4e445b99eb37ef0caf0b94952397a763856c19c
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-14 16:20:26 +0000

    Tue, Jan 14, 2025, 11:20 AM -05:00

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -10,9 +10,9 @@ import Image from "next/image";
 import blossm1 from "./blossm/image2.png";
 import blossm2 from "./blossm/image4.png";
 import blossm4 from "./blossm/image5.png";
-import volume1 from "./volume/image2.png";
-import volume2 from "./volume/image4.png";
-import volume3 from "./volume/image1.png";
+import volume1 from "./volume/image1.png";
+import volume2 from "./volume/image2.png";
+import volume3 from "./volume/image3.png";
 
 export default function Home() {
   return (
@@ -41,8 +41,8 @@ export default function Home() {
                 The Blossm Extension is a tool installed directly onto the
                 Google Chrome web browser. It is a simple and alternative method
                 for Blossm users to securely verify their OnlyFans account
-                ownership to display the stats on their Blossm profile: <br />-
-                Top % <br />- # of Fans (Subscribers) <br />- # of Profiles
+                ownership to display the stats on their Blossm profile: <br />•
+                Top % <br />• # of Fans (Subscribers) <br />• # of Profiles
                 Following. <br />
                 <br /> The extension uses an API Key to get the information. API
                 stands for Application Programming Interface and this provides a
@@ -73,35 +73,28 @@ export default function Home() {
                 Volume.com
               </Typography>
               <Typography variant="p" component="p">
-                The Blossm Extension is a tool installed directly onto the
-                Google Chrome web browser. It is a simple and alternative method
-                for Blossm users to securely verify their OnlyFans account
-                ownership to display the stats on their Blossm profile: <br />-
-                Top % <br />- # of Fans (Subscribers) <br />- # of Profiles
-                Following. <br />
-                <br /> The extension uses an API Key to get the information. API
-                stands for Application Programming Interface and this provides a
-                secure and standardized way for applications to work together.
-                Simply put, the API key allows your OnlyFans page to talk
-                directly to your Blossm account! This does not require you to
-                enter your login credentials on Blossm.com, but you do need to
-                be signed into your OnlyFans account within your browser for the
-                extension to work.
+                Explore the innovative redesign of the Volume.com profile stream
+                page, crafted to deliver an intuitive and engaging user
+                experience. The new design highlights:
+                <br />
+                <br />• Enhanced Video Player: Stream videos with high-quality
+                options, complete with seamless Chromecast and Apple AirPlay
+                integration for versatile viewing across devices.
+                <br />• Interactive Chat Feature: A collapsible chat panel lets
+                you connect with other viewers while keeping the video in focus
+                or switch to full-screen mode for an immersive experience.
+                <br />• Dynamic Profile Integration: Scroll down effortlessly to
+                explore detailed user profiles, fostering connections and
+                community engagement right from the stream page.
+                <br />
+                <br />
+                This redesign prioritizes accessibility, performance, and user
+                satisfaction, making streaming and interaction smoother and more
+                enjoyable than ever.
               </Typography>
-              <Box
-                mt={8}
-                sx={{
-                  width: "100%",
-                  display: "grid",
-                  gridTemplateColumns:
-                    "repeat(auto-fill, minmax(min(285px, 100%), 1fr))",
-                  gap: 2,
-                }}
-              >
-                <Image src={blossm1} height="300" />
-                <Image src={blossm2} height="300" />
-                <Image src={blossm4} height="300" />
-              </Box>
+              <Image src={volume1} height="300" />
+              <Image src={volume2} height="300" />
+              <Image src={volume3} height="300" />
             </Grid>
           </Grid>
         </Box>
