7779d1345a23b40451730b6f87dabdf79e840364
David Mascia <753866+dmascia@users.noreply.github.com>
2025-01-14 17:18:45 +0000

    Tue, Jan 14, 2025, 12:18 PM -05:00

--- src/app/blog/how-to-create-a-nextjs-app-a-step-by-step-guide/page.tsx
+++ src/app/blog/how-to-create-a-nextjs-app-a-step-by-step-guide/page.tsx
@@ -122,14 +122,14 @@ export default function Home() {
                   <Paper sx={{ backgroundColor: grey[900] }}>
                     <Box p={1}>
                       <Typography variant="p" sx={{ color: grey[50] }}>
-                      {export default function About() {
+                        {"export default function About() {
   return (
     <div>
       <h1>About Us</h1>
       <p>This is the about page of our Next.js app.</p>
     </div>
-  );}
-}
+  )
+} "}
                       </Typography>
                     </Box>
                   </Paper>
